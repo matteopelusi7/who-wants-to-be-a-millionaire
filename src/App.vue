8aa5372ca44be1b00f61748a90ce58dcfb9eec67
matteopelusi7 <pelusimatteo02@gmail.com>
2022-08-29 12:02:44 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+
+    <MainHome />
+
   </div>
 </template>
 
+<script>
+
+import MainHome from './components/MainHome.vue'
+export default {
+  name: 'App',
+  components: {
+    MainHome,
+  },
+}
+
+</script>
+
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  margin: -8px;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/MainHome.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+
+    <div class="header">
+      <figure class="image-logo">
+        <img src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0b8e1102-7ff4-4870-a39e-ef625253af75%2Flogo.png?table=block&id=d1419994-97c4-4690-b349-03db5eada926&spaceId=4c49ff97-016b-4669-8de5-4479dd1a86e1&width=480&userId=863850ea-3b24-4de8-b681-d214b9ffd42e&cache=v2" alt="">
+      </figure>
+    </div>
+
+    <div class="footer">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainHome',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.home {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  height: 40%;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .image-logo {
+    img {
+      width: 300px;
+    }
+  }
+}
+
+.footer {
+  height: 60%;
+  background-color: rgb(1, 1, 77);
+}
+
+</style>
